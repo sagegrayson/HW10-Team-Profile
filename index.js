96bfd289ad593ae24d61962dcaf0482f027e1484
skg <skaseyg@gmail.com>
2021-12-30 19:47:19 -0500

    finished example html

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// 
+// DEPENDENCIES ===========================================
+
+const inquirer = require("inquirer");
+const fs = requrire("fs");
+
+const Manager = require("./lib/manager.js");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+
+// VARS ===================================================
+
+const employees = [];
+
+// FUNCTIONS ==============================================
+
+
+
+// INITIAL ================================================
