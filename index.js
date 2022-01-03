5ab516142f1434455f49affbe45a897bc39c13f5
skg <skaseyg@gmail.com>
2022-01-03 00:57:56 -0500

    Inquirer Finished

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const fs = require("fs");
 const Manager = require("./lib/manager.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
+const { Console } = require("console");
 
 // VARS ===================================================
 
 const employees = [];
 
-// FUNCTIONS ==============================================
-
-function topHTML() {
-	const topHTML = `
+const topHTML = `
     <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -26,18 +24,22 @@ function topHTML() {
         <body>
             <header>My Team</header>`;
 
+const bottomHTML = `
+		</body>
+	</html>`;
+
+// FUNCTIONS ==============================================
+
+function generate(employees) {
+	// Top HTML
 	fs.writeFile("./dist/output.html", topHTML, function (err) {
 		if (err) {
 			console.log(err);
 		}
 	});
-}
 
-function bottomHTML() {
-	const bottomHTML = `
-    	</body>
-    </html>
-    `;
+	
+
 	fs.appendFile("./dist/output.html", bottomHTML, function (err) {
 		if (err) {
 			console.log(err);
@@ -46,31 +48,81 @@ function bottomHTML() {
 }
 
 // Inquirer ----------------------------------------------
-function generate() {
-	inquirer.prompt([
-		{
-			type: "input",
-			message: "Employee ID?",
-			name: "empID",
-		},
-		{
-			type: "input",
-			message: "Employee Name?",
-			name: "empName",
-		},
-		{
-			type: "input",
-			message: "Employee Email?",
-			name: "empEmail",
-		},
-		{
-			type: "list",
-			message: "Employee Role?",
-			choices: ["Manager", "Engineer", "Intern"],
-			name: "empRole",
-		},
-	]);
+function getInfo() {
+	inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "Employee ID?",
+				name: "empID",
+			},
+			{
+				type: "input",
+				message: "Employee Name?",
+				name: "empName",
+			},
+			{
+				type: "input",
+				message: "Employee Email?",
+				name: "empEmail",
+			},
+			{
+				type: "list",
+				message: "Employee Role?",
+				choices: ["Manager", "Engineer", "Intern"],
+				name: "empRole",
+			},
+		])
+		.then((answer) => {
+			let promptMessage;
+			switch (answer.empRole) {
+				case "Manager":
+					promptMessage = "Office number";
+					break;
+
+				case "Engineer":
+					promptMessage = "github username";
+					break;
+
+				case "Intern":
+					promptMessage = "school";
+					break;
+
+				default:
+					console.log("how did you even do that?");
+			}
+
+			inquirer
+				.prompt([
+					{
+						type: "input",
+						message: `What is ${answer.empName}'s ${promptMessage}?`,
+						name: "empInfo",
+					},
+					{
+						type: "confirm",
+						message: "Is there another employee?",
+						name: "return",
+					},
+				])
+				.then((response) => {
+					employees.push({
+						id: answer.empID,
+						name: answer.empName,
+						email: answer.empEmail,
+						role: answer.empRole,
+						info: response.empInfo,
+					});
+
+					if (response.return) {
+						generate();
+					} else {
+						console.log(employees);
+						generate(employees);
+					}
+				});
+		});
 }
 
 // INITIAL ================================================
-generate();
+getInfo();
