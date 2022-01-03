9fc582ff570a158452a4bd0bf2bfa118ac3966a9
skg <skaseyg@gmail.com>
2022-01-03 16:32:13 -0500

    styled example, changed index.js html

--- index.js
+++ index.js
@@ -12,20 +12,42 @@ const Intern = require("./lib/intern.js");
 const employees = [];
 
 const top = `
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Team Profile</title>
-        </head>
-        <body>
-            <header>My Team</header>`;
-
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Team Profile</title>
+		<link
+			href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+			rel="stylesheet"
+			integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+			crossorigin="anonymous"
+		/>
+	</head>
+	<body class="bg-light">
+		<nav class="navbar navbar-dark bg-dark">
+			<div class="container-fluid">
+				<span class="navbar-brand mb-0 h1">My Team</span>
+			</div>
+		</nav>
+		<main class="album py-5">
+			<div class="container">
+				<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+				`;
 const fin = `
-		</body>
-	</html>`;
+				</div>
+			</div>
+		</main>
+		<script
+			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+			crossorigin="anonymous"
+		></script>
+	</body>
+</html>
+`;
 
 // FUNCTIONS ==============================================
 
@@ -55,17 +77,27 @@ function midHTML(employees) {
 			case "Manager":
 				let office = employee.getOfficeNumber();
 				toAppend = `
-				<div>
-					<h1>${name}</h1>
-					<h3>Manager</h3>
-					<ul>
-						<li>ID: ${id}</li>
-						<li>
-							Email:
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">${name}</h5>
+							<h6 class="card-subtitle mb-2 text-muted">
+								Manager
+							</h6>
+							<p class="card-text">
+							[Placeholder About Text]
+							</p>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">ID: ${id}</li>
+							<li class="list-group-item">
+								Office Number: ${office}
+							</li>
+							<li class="list-group-item">
 							<a href="mailto:${email}">${email}</a>
-						</li>
-						<li>Office Number: ${office}</li>
-					</ul>
+							</li>
+						</ul>
+					</div>
 				</div>
 				`;
 				break;
@@ -73,19 +105,28 @@ function midHTML(employees) {
 			case "Engineer":
 				let github = employee.getGitHub();
 				toAppend = `
-				<div>
-					<h1>${name}</h1>
-					<h3>Engineer</h3>
-					<ul>
-						<li>ID: ${id}</li>
-						<li>
-							Email:
-							<a href="mailto:${email}">${email}</a>					</li>
-						<li>
-							Github:
-							<a href="http://www.github.com/${github}">${github}</a>
-						</li>
-					</ul>
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">${name}</h5>
+							<h6 class="card-subtitle mb-2 text-muted">
+								Engineer
+							</h6>
+							<p class="card-text">
+							[Placeholder About Text]
+							</p>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">ID: ${id}</li>
+							<li class="list-group-item">
+								Github:
+								<a href="http://www.github.com/${github}">${github}</a>
+							</li>
+							<li class="list-group-item">
+								<a href="mailto:${email}">${email}</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 				`;
 				break;
@@ -93,17 +134,25 @@ function midHTML(employees) {
 			case "Intern":
 				let school = employee.getSchool();
 				toAppend = `
-				<div>
-					<h1>${name}</h1>
-					<h3>Intern</h3>
-					<ul>
-						<li>ID: ${id}</li>
-						<li>
-							Email:
-							<a href="mailto:${email}">${email}</a>					</li>
-						</li>
-						<li>School: ${school}</li>
-					</ul>
+				<div class="col">
+					<div class="card">
+						<div class="card-body">
+							<h5 class="card-title">${name}</h5>
+							<h6 class="card-subtitle mb-2 text-muted">
+								Intern
+							</h6>
+							<p class="card-text">
+							[Placeholder About Text]
+							</p>
+						</div>
+						<ul class="list-group list-group-flush">
+							<li class="list-group-item">ID: ${id}</li>
+							<li class="list-group-item">School: ${school}</li>
+							<li class="list-group-item">
+								<a href="mailto:${email}">${email}</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 				`;
 				break;
