cc332d54e27197e22e7fbb4d486ddc906deb6ea6
skg <skaseyg@gmail.com>
2022-01-03 04:14:36 -0500

    working product

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Intern = require("./lib/intern.js");
 
 const employees = [];
 
-const topHTML = `
+const top = `
     <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -23,33 +23,110 @@ const topHTML = `
         <body>
             <header>My Team</header>`;
 
-const bottomHTML = `
+const fin = `
 		</body>
 	</html>`;
 
 // FUNCTIONS ==============================================
 
-function generate(employees) {
-	// Top HTML
-	// fs.writeFile("./dist/output.html", topHTML, function (err) {
-	// 	if (err) {
-	// 		console.log(err);
-	// 	}
-	// });
+// generation ---------------------------------------------
 
+function topHTML() {
+	fs.writeFile("./dist/output.html", top, function (err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+	midHTML(employees);
+}
+
+function midHTML(employees) {
 	for (let i = 0; i < employees.length; i++) {
 		const employee = employees[i];
-		console.log(employee.constructor.name);
-		if (employee.constructor.name === "Manager"){
-			console.log("nice!")
-		}
-	}
 
-	// fs.appendFile("./dist/output.html", bottomHTML, function (err) {
-	// 	if (err) {
-	// 		console.log(err);
-	// 	}
-	// });
+		let role = employee.getRole();
+		let id = employee.getId();
+		let name = employee.getName();
+		let email = employee.getEmail();
+
+		let toAppend = ``;
+
+		switch (role) {
+			case "Manager":
+				let office = employee.getOfficeNumber();
+				toAppend = `
+				<div>
+					<h1>${name}</h1>
+					<h3>Manager</h3>
+					<ul>
+						<li>ID: ${id}</li>
+						<li>
+							Email:
+							<a href="mailto:${email}">${email}</a>
+						</li>
+						<li>Office Number: ${office}</li>
+					</ul>
+				</div>
+				`;
+				break;
+
+			case "Engineer":
+				let github = employee.getGitHub();
+				toAppend = `
+				<div>
+					<h1>${name}</h1>
+					<h3>Engineer</h3>
+					<ul>
+						<li>ID: ${id}</li>
+						<li>
+							Email:
+							<a href="mailto:${email}">${email}</a>					</li>
+						<li>
+							Github:
+							<a href="http://www.github.com/${github}">${github}</a>
+						</li>
+					</ul>
+				</div>
+				`;
+				break;
+
+			case "Intern":
+				let school = employee.getSchool();
+				toAppend = `
+				<div>
+					<h1>${name}</h1>
+					<h3>Intern</h3>
+					<ul>
+						<li>ID: ${id}</li>
+						<li>
+							Email:
+							<a href="mailto:${email}">${email}</a>					</li>
+						</li>
+						<li>School: ${school}</li>
+					</ul>
+				</div>
+				`;
+				break;
+
+			default:
+				break;
+		}
+
+		fs.appendFile("./dist/output.html", toAppend, function (err) {
+			if (err) {
+				console.log(err);
+			}
+		});
+	}
+	finHTML();
+}
+
+function finHTML() {
+	fs.appendFile("./dist/output.html", fin, function (err) {
+		if (err) {
+			console.log(err);
+		}
+	});
 }
 
 // Inquirer ----------------------------------------------
@@ -151,7 +228,7 @@ function getInfo() {
 						getInfo();
 					} else {
 						// console.log(employees);
-						generate(employees);
+						topHTML();
 					}
 				});
 		});
